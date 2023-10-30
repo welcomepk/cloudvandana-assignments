f75037cf04e87dca4f6355d06f2f06f516976391
welcomepk <kamblepramod806@gmail.com>
2023-10-30 19:05:00 +0530

    shuffle array completed

---
+++ java-assignments/ShuffleArray.java
@@ -0,0 +1,24 @@
+public class ShuffleArray {
+
+    public static void shuffleArray(int[] arr) {
+
+        int len = arr.length;
+
+        for (int i = 0; i < arr.length; i++) {
+            int randomIndex = i + (int) (Math.random() * (len - i));
+            int tmp = arr[i];
+            arr[i] = arr[randomIndex];
+            arr[randomIndex] = tmp;
+        }
+        // dsf
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        shuffleArray(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
